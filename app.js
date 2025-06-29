c0740d043690b556e7634165eb7c005c08840149
alideveloper15 <172058104+alideveloper15@users.noreply.github.com>
2025-06-29 16:22:18 +0500

    Added addEventListener

--- app.js
+++ app.js
@@ -14,10 +14,7 @@ function renderTodoList(){
         const html = `
             <div>${name}</div>
             <div>${dueDate}</div>
-            <button onclick="
-                todoList.splice(${index}, 1);
-                renderTodoList();
-            " class="delete-todo-button">Delete</button>
+            <button class="delete-todo-button js-delete-todo-button">Delete</button>
             `;
         todoListHTML +=html
     })
@@ -25,9 +22,20 @@ function renderTodoList(){
     document.querySelector('.js-todo-list')
         .innerHTML= todoListHTML
 
+    document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton, index) => {
+        deleteButton.addEventListener('click', () => {
+            todoList.splice(index, 1)
+            renderTodoList()
+        })
+    })
+
     localStorage.setItem('todoList',JSON.stringify(todoList))
 }
 
+document.querySelector('.js-add-todo-button')
+    .addEventListener('click', () => {
+        addTodo()
+    })
 
 function addTodo(){
     const inputElement = document.querySelector('.js-name-input')
